2df30329a8777857c7fe4a198412e2b2d5a1200d
Felipe-Gabriel-Souza-Goncalves <lipebiel35@gmail.com>
2026-04-19 16:55:07 -0300

    v1.0.1 - Python, correções/otimizações/loop de jogo

--- Python/main.py
+++ Python/main.py
@@ -1,51 +1,70 @@
 import random, os
 
-pontuacao = 0
-tentativas = 0
-alcance = 10
-arrayPalpites = []
+jogando = True
+while (jogando):
 
-print("qual a dificuldade? (insira o número correspondente) \n")
-print("1- Fácil: 0 - 10")
-print("2 -Médio: 0 - 100")
-print("3 -Difícil: 0 - 1000 \n")
-dificuldade = input()
-match(dificuldade):
-    case "1":
-        alcance = 10
-    case "2":
-        alcance = 100
-    case "3":
-        alcance = 1000
+    pontuacao = 0
+    tentativas = 1
+    alcance = 10
+    arrayPalpites = []
 
-os.system("cls")
+    print("Qual a dificuldade? (insira o número correspondente) \n")
+    print("1 - Fácil: 0 - 10")
+    print("2 - Médio: 0 - 100")
+    print("3 - Difícil: 0 - 1000 \n")
+    dificuldade = input()
 
+    match(dificuldade):
+        case "1":
+            alcance = 10
+        case "2":
+            alcance = 100
+        case "3":
+            alcance = 1000
 
-numero = random.randint(0, alcance)
+    os.system("cls")
 
-while(True):
-    try:
-        print("Tentativa ", tentativas, " Palpites: ", arrayPalpites)
-        palpite = int(input())
-    except ValueError:
-        print("input inválido")
-        continue
+    numero = random.randint(0, alcance)
 
-    if(palpite < numero and palpite > 0):
-        print("errou, o número é maior!")
-        arrayPalpites.append(palpite)
+    # Loop do jogo
+    while(True):
+        try:
+            print("Tentativa ", tentativas, " Palpites: ", arrayPalpites)
+            palpite = input()
 
-    elif(palpite > numero and palpite < alcance):
-        print("errou, o número é menor!")
-        arrayPalpites.append(palpite)
+            if(palpite == "limpar"):
+                os.system("cls")
+                continue
 
-    elif(palpite == numero):
-        print("Parabéns você acertou!")
-        print("Tentativas: ", tentativas)
-        print("Palpite: ", palpite)
-        print("Numero: ", numero)
-        break
-    else:
-        print("input inválido")
-        continue
-    tentativas+=1
+            palpite = int(palpite)
+            
+        except ValueError:
+            print("input inválido")
+            continue
+
+        if(palpite < numero and palpite > 0):
+            print("Errou, o número é maior!")
+            arrayPalpites.append(palpite)
+
+        elif(palpite > numero and palpite < alcance):
+            print("Errou, o número é menor!")
+            arrayPalpites.append(palpite)
+
+        elif(palpite == numero):
+            print("Parabéns você acertou!")
+            print("Tentativas: ", tentativas)
+            print("Palpite: ", palpite)
+            print("Número: ", numero, "\n")
+
+            continuar = input("Deseja continuar jogando? (s/n)")
+            if(continuar == "s"):
+                os.system("cls")
+                tentativas = 1
+                break
+            else:
+                jogando = False
+                break
+        else:
+            print("input inválido")
+            continue
+        tentativas+=1
