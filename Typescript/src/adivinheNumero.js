ac59a55ca778df3090a857646b17b8cf3dd5c525
Felipe-Gabriel-Souza-Goncalves <lipebiel35@gmail.com>
2026-04-24 19:36:03 -0300

    v1.0.1 - Typescript - 1ª versão funcional

--- Typescript/src/adivinheNumero.js
+++ Typescript/src/adivinheNumero.js
@@ -1,54 +1,87 @@
+// Declaram quais dificuldades podem haver no jogo
+var dificuldades = ["Fácil", "Médio", "Difícil"];
+var dificuldade = null; // Null até iniciar uma partida
+var container = document.getElementById("containerDificuldade");
+container === null || container === void 0 ? void 0 : container.querySelectorAll("button").forEach(function (button) {
+    button.addEventListener("click", function () {
+        var valor = button.textContent;
+        if (dificuldades.includes(valor)) {
+            dificuldade = valor;
+            iniciar();
+        }
+    });
+});
 var inputPalpite = document.getElementById("inputPalpite");
 var botaoEnviar = document.getElementById("botaoEnviar");
+var statusTentativa = document.getElementById("statusTentativa");
 var spanTentativas = document.getElementById("spanTentativas");
 var tentativas = 0;
-var resposta = null;
+var resposta = 0;
+// Desativa o botão de enviar se o input estiver vazio
 inputPalpite.addEventListener("input", function () {
     var value = inputPalpite.value;
-    value === "" ? botaoEnviar.disabled = true : botaoEnviar.disabled = false;
+    value === "" ? (botaoEnviar.disabled = true) : (botaoEnviar.disabled = false);
 });
+// Envia o palpite para verificação
 botaoEnviar.addEventListener("click", function () {
     if (inputPalpite.value !== "") {
-        verificarNumero(parseInt(inputPalpite.value));
+        var acertou = verificarNumero(parseInt(inputPalpite.value));
+        if (acertou) {
+            ganhar();
+        }
     }
 });
-function iniciar(dificuldade) {
+function iniciar() {
     // Reinicia tentativas e atualiza DOM
     tentativas = 0;
     limparDOM();
     // Gera o número para adivinhar e limita o input de palpite
     switch (dificuldade) {
-        case 1:
+        case "Fácil":
             resposta = gerarNumero(10);
             inputPalpite.max = "10";
             break;
-        case 2:
+        case "Médio":
             resposta = gerarNumero(100);
             inputPalpite.max = "100";
             break;
-        case 3:
+        case "Difícil":
             resposta = gerarNumero(1000);
             inputPalpite.max = "1000";
             break;
     }
 }
-// Cria a resposta do jogo com base no alcance 10/100/10000=
+// Cria a resposta do jogo com base no alcance 10/100/10000
 function gerarNumero(alcance) {
     var numero = Math.floor(Math.random() * alcance);
     return numero;
 }
 // Deixa o DOM no estado de começo do jogo
 function limparDOM() {
+    container.style.display = "none";
+    statusTentativa.textContent = "";
     spanTentativas.textContent = "0";
     inputPalpite.value = "";
     inputPalpite.focus();
 }
 function verificarNumero(palpite) {
     tentativas += 1;
+    // Lógica de comparação
     if (palpite === resposta) {
+        statusTentativa.textContent = "ACERTOU!!";
         return true;
     }
+    else if (palpite > -1 && palpite < resposta) {
+        statusTentativa.textContent = "O palpite é menor que a resposta!";
+    }
+    else if (palpite > resposta && palpite < parseInt(inputPalpite.max)) {
+        statusTentativa.textContent = "O palpite é maior que a resposta!";
+    }
+    spanTentativas.textContent = tentativas.toString();
     inputPalpite.value = "";
     inputPalpite.focus();
     return false;
+}
+function ganhar() {
+    container.style.display = "block";
 }
--- Typescript/src/adivinheNumero.ts
+++ Typescript/src/adivinheNumero.ts
@@ -1,65 +1,102 @@
-const inputPalpite = document.getElementById("inputPalpite") as HTMLInputElement
-const botaoEnviar = document.getElementById("botaoEnviar") as HTMLInputElement
-const spanTentativas = document.getElementById("spanTentativas") as HTMLElement
+// Declaram quais dificuldades podem haver no jogo
+const dificuldades = ["Fácil", "Médio", "Difícil"] as const;
+type Dificuldade = typeof dificuldades[number]
+let dificuldade: Dificuldade | null = null // Null até iniciar uma partida
+
+const container = document.getElementById("containerDificuldade") as HTMLElement;
+container?.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+
+    const valor = button.textContent as Dificuldade;
+     
+    if (dificuldades.includes(valor)) {
+      dificuldade = valor;
+      iniciar();
+    }
+  });
+});
+
+const inputPalpite = document.getElementById("inputPalpite") as HTMLInputElement;
+const botaoEnviar = document.getElementById("botaoEnviar") as HTMLInputElement;
+
+const statusTentativa = document.getElementById("statusTentativa") as HTMLElement;
+const spanTentativas = document.getElementById("spanTentativas") as HTMLElement;
 
 let tentativas: number = 0;
-let resposta: number | null = null;
+let resposta: number = 0;
 
-inputPalpite.addEventListener("input", () =>{
-  const value = inputPalpite.value
-  value === "" ? botaoEnviar.disabled = true : botaoEnviar.disabled = false 
-})   
+// Desativa o botão de enviar se o input estiver vazio
+inputPalpite.addEventListener("input", () => {
+  const value = inputPalpite.value;
+  value === "" ? (botaoEnviar.disabled = true) : (botaoEnviar.disabled = false);
+});
 
-botaoEnviar.addEventListener("click", () =>{
-  if(inputPalpite.value !== ""){
-    verificarNumero(parseInt(inputPalpite.value))
+// Envia o palpite para verificação
+botaoEnviar.addEventListener("click", () => {
+  if (inputPalpite.value !== "") {
+    const acertou: boolean = verificarNumero(parseInt(inputPalpite.value));
+    if (acertou) {
+      ganhar();
+    }
   }
-})
+});
 
-function iniciar(dificuldade: number): void{
+function iniciar(): void {
   // Reinicia tentativas e atualiza DOM
   tentativas = 0;
-  limparDOM()
+  limparDOM();
 
   // Gera o número para adivinhar e limita o input de palpite
-  switch(dificuldade){
-    case 1: 
-      resposta = gerarNumero(10)
-      inputPalpite.max = "10"
-      break
-    case 2: 
-      resposta = gerarNumero(100)
-      inputPalpite.max = "100"
-      break
-    case 3: 
-      resposta = gerarNumero(1000)
-      inputPalpite.max = "1000"
-      break
+  switch (dificuldade) {
+    case "Fácil":
+      resposta = gerarNumero(10);
+      inputPalpite.max = "10";
+      break;
+    case "Médio":
+      resposta = gerarNumero(100);
+      inputPalpite.max = "100";
+      break;
+    case "Difícil":
+      resposta = gerarNumero(1000);
+      inputPalpite.max = "1000";
+      break;
   }
-
 }
 
-// Cria a resposta do jogo com base no alcance 10/100/10000=
-function gerarNumero(alcance: number): number{
-  const numero = Math.floor(Math.random()*alcance)
-  return numero
+// Cria a resposta do jogo com base no alcance 10/100/10000
+function gerarNumero(alcance: number): number {
+  const numero = Math.floor(Math.random() * alcance);
+  return numero;
 }
 
 // Deixa o DOM no estado de começo do jogo
-function limparDOM(){
-  spanTentativas.textContent = "0"
-  inputPalpite.value = ""
-  inputPalpite.focus()
+function limparDOM() {
+  container.style.display = "none";
+  statusTentativa.textContent = ""
+  spanTentativas.textContent = "0";
+  inputPalpite.value = "";
+  inputPalpite.focus();
 }
 
-function verificarNumero(palpite: number): boolean{
+function verificarNumero(palpite: number): boolean {
   tentativas += 1;
-  if(palpite === resposta){
-    return true
+
+  // Lógica de comparação
+  if (palpite === resposta) {
+    statusTentativa.textContent = "ACERTOU!!";
+    return true;
+  } else if (palpite > -1 && palpite < resposta) {
+    statusTentativa.textContent = "O palpite é menor que a resposta!";
+  } else if (palpite > resposta && palpite < parseInt(inputPalpite.max)) {
+    statusTentativa.textContent = "O palpite é maior que a resposta!";
   }
 
-  inputPalpite.value = ""
-  inputPalpite.focus()
-  return false
+  spanTentativas.textContent = tentativas.toString();
+  inputPalpite.value = "";
+  inputPalpite.focus();
+  return false;
 }
 
+function ganhar(): void {
+  container.style.display = "block"
+}
